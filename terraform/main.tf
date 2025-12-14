064ab7870c146349b17142818403b8bf964cfc25
ichrakismail289-max <ichrakismail289@gmail.com>
2025-12-15 00:52:53 +0100

    cleanup: remove venv and terraform cache

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,28 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "Telmate/proxmox"
-      version = "2.9.7"
-    }
-  }
-}
-
 provider "proxmox" {
-  pm_api_url      = "https://192.168.17.134:8006/api2/json"
+  pm_api_url      = "https://192.168.17.131:8006/api2/json"
   pm_user         = "root@pam"
-  pm_password     = "TON_MOT_DE_PASSE"
+  pm_password     = "ichrak1234"
   pm_tls_insecure = true
 }
 
 resource "proxmox_lxc" "ct_base" {
-  hostname    = "ct-base"
-  target_node = "ct-base"  # <-- le node de ton Proxmox
-  ostemplate  = "local:vztmpl/debian-12-standard_12.12-1_amd64.tar.gz"
-  memory      = 2048
-  cores       = 2
+  hostname   = "ct-base"
+  target_node = "pve"  # Remplace par ton node
+  ostemplate = "local:vztmpl/debian-12-standard_12.12-1_amd64.tar.gz"
+  password   = "ichrak1234"       # mot de passe root du LXC
+  cores      = 2
+  memory     = 2048
 
   rootfs {
-    size = "10G"
+    storage = "local-lvm"  # ← Indique ton storage Proxmox
+    size    = "8G"
   }
 
   network {
-    name    = "eth0"
-    bridge  = "vmbr0"
-    ip      = "dhcp"
+    name = "eth0"
+    bridge = "vmbr0"
+    ip = "192.168.100.134/24"
+    type = "veth"
+    gateway = "192.168.17.134"
   }
 }
